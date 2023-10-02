51eb64370bc46c740fb18b12fdc1406c1118d06b
Bravin <onsasebravin853@gmail.com>
2023-10-02 12:35:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Bravin';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, BRAVIN.toUppercase!`;
+const shortGreeting = `Welcome, B.slice(0,1)!`;
